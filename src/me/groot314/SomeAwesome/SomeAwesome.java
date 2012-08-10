1aaf1304642f2d1dfce5caec5145bc96a826bb93
Geoffrey <groot314@gmail.com>
2012-08-09 18:34:31 -0700

    First Commit

---
+++ src/me/groot314/SomeAwesome/SomeAwesome.java
@@ -0,0 +1,15 @@
+package me.groot314.SomeAwesome;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SomeAwesome extends JavaPlugin{
+
+	@Override
+	public void onEnable() {
+
+	}
+	
+	@Override
+	public void onDisable() {
+	}
+}
